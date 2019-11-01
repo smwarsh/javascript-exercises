8617b749c8e88a9899ab7c360f55e56f2274a88a
Steph Warsh <swarsh@u.rochester.edu>
2019-11-01 15:02:00 -0400

    I was 100% correct! Hooray, I know so much about var/let/const now

--- react-course/101-es5-warmup/var-example.js
+++ react-course/101-es5-warmup/var-example.js
@@ -1,9 +1,9 @@
-log(name); // output: Reference Error
+// log(name); // output: Uncaught ReferenceError: Cannot access 'name' before initialization
 
 const tinyize = function(name) {
   log(name); // output: Line 4: Rick
 
-  log(myName); // output: Line 6: Reference Error
+  // log(myName); // output: Uncaught ReferenceError: Cannot access 'myName' before initialization
   const myName = "Tiny " + name + "!";
   log(myName); // output: Line 8: Tiny Rick!
 
@@ -12,15 +12,15 @@ const tinyize = function(name) {
 
 const name = tinyize("Rick");
 log(name); // output: Line 14: Tiny Rick!
-log(myName); // output: Reference Error
+// log(myName); // output: Uncaught ReferenceError: myName is not defined
 
-log(i); // output: Reference Error
+// log(i); // output: Uncaught ReferenceError: i is not defined
 for (let i = 0; i < 5; i++) {
   setTimeout(function() {
     log(i); // output: Line 20: 0 / Line 20: 1 / Line 20: 2 / Line 20: 3 / Line 20: 4
   }, 100);
 }
-log(i); // output: Reference Error
+// log(i); // output: Uncaught ReferenceError: i is not defined
 
 // helper function to log current line number and the passed in string
 // e.g. "Line 24: Hello World"
