7018b1dbbaf31318c6c9ee8527451e5ed5987a1a
Steph Warsh <swarsh@u.rochester.edu>
2019-11-01 12:20:07 -0400

    Add expected output to comments in place of ?s

--- react-course/101-es5-warmup/var-example.js
+++ react-course/101-es5-warmup/var-example.js
@@ -1,26 +1,26 @@
-log(name); // output: ?
+log(name); // output: Line 1: null
 
 var tinyize = function(name) {
-  log(name); // output: ?
+  log(name); // output: Line 4: Rick
 
-  log(myName); // output: ?
+  log(myName); // output: Line 6: null
   var myName = "Tiny " + name + "!";
-  log(myName); // output: ?
+  log(myName); // output: Line 8: Tiny Rick!
 
   return myName;
 };
 
 var name = tinyize("Rick");
-log(name); // output: ?
-log(myName); // output: ?
+log(name); // output: Line 14: Tiny Rick!
+log(myName); // output: Line 15: null
 
-log(i); // output: ?
+log(i); // output: Line 17: null
 for (var i = 0; i < 5; i++) {
   setTimeout(function() {
-    log(i); // output: ?
+    log(i); // output: Line 20: 0 / Line 20: 1 / Line 20: 2 / Line 20: 3 / Line 20: 4
   }, 100);
 }
-log(i); // output: ?
+log(i); // output: Line 23: null
 
 // helper function to log current line number and the passed in string
 // e.g. "Line 24: Hello World"
