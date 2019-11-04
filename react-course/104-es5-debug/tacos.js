1a930107862503f1d521a080912c3ce33fcc77b4
Steph Warsh <swarsh@u.rochester.edu>
2019-11-04 13:20:25 -0500

    Fix bug by changing all instances of the var keyword to const or let

--- react-course/104-es5-debug/tacos.js
+++ react-course/104-es5-debug/tacos.js
@@ -1,15 +1,17 @@
-var classmates = ["Christian", "CJ", "Tammer"];
+const classmates = ["Christian", "CJ", "Tammer"];
 
-var wrapper = document.getElementById("wrapper");
+const wrapper = document.getElementById("wrapper");
 
-for (var i = 0; i < classmates.length; i++) {
-  var classmate = classmates[i];
+for (let i = 0; i < classmates.length; i++) {
+  // can use const for classmate and button because the scope is only this iteration of the for loop
+  const classmate = classmates[i];
 
-  var button = document.createElement("button");
+  // doesn't work with var because button is used with the global value of the last item passed through in the for loop
+  const button = document.createElement("button");
   button.innerHTML = "How many tacos did " + classmate + " eat?";
 
   button.addEventListener("click", function() {
-    var tacoCount = i * 2;
+    const tacoCount = i * 2;
 
     alert(classmate + " ate " + tacoCount + " tacos.");
 
