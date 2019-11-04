97511d83b3691901b0eda44bcdb5bdad1fa33f8f
Steph Warsh <swarsh@u.rochester.edu>
2019-11-04 13:44:53 -0500

    Change var to const everywhere + change string concatenation to template literals

--- react-course/105-game-of-strings/game-of-strings.js
+++ react-course/105-game-of-strings/game-of-strings.js
@@ -1,26 +1,26 @@
-var houses = ['Stark', 'Lannister', 'Targaryen', 'Baratheon'];
+const houses = ['Stark', 'Lannister', 'Targaryen', 'Baratheon'];
 
-var getRandomHouse = function (houses) {
+const getRandomHouse = function (houses) {
   return houses[Math.floor(Math.random() * houses.length)];
 }
 
-var getMove = function () {
+const getMove = function () {
   // clone the array, pull an attacker and a defender from it
-  var housesLeft = houses.slice(0);
-  var defender = getRandomHouse(housesLeft);
+  const housesLeft = houses.slice(0);
+  const defender = getRandomHouse(housesLeft);
   housesLeft.splice(housesLeft.indexOf(defender), 1);
-  var attacker = getRandomHouse(housesLeft);
+  const attacker = getRandomHouse(housesLeft);
 
   // return an object representing the the attacker and defender state
   return {
-    text: '  House ' + defender + ' infringed on the ' + attacker + 's\' honor.\n' +
-    '  As a result, house ' + attacker + ' has decided to attack!',
+    text: `  House ${defender} infringed on the ${attacker}s' honor.
+    As a result, house ${attacker} has decided to attack!`,
     attacker: attacker,
     defender: defender
   };
 };
 
-var getLoser = function (move) {
+const getLoser = function (move) {
   if (move.text.indexOf('House Targaryen') === 0) {
     // Fire and Blood.
     return move.defender;
@@ -39,19 +39,19 @@ var getLoser = function (move) {
   return move.defender;
 }
 
-var playGame = function () {
-  var move = getMove();
-  console.log('Move:\n' + move.text);
+const playGame = function () {
+  const move = getMove();
+  console.log(`Move:\n${move.text}`);
 
-  var loser = getLoser(move);
+  const loser = getLoser(move);
   houses.splice(houses.indexOf(loser), 1);
 
-  console.log('\n  Loser: ' + loser + '\n  Houses Left: ' + houses.join(', ') + '\n  ---\n');
+  console.log(`\n  Loser: ${loser}\n  Houses Left: ${houses.join(', ')}\n  ---\n`);
 }
 
 while (houses.length > 1) {
   playGame();
 }
 
-console.log('Winner: House ' + houses[0]);
+console.log(`Winner: House ${houses[0]}`);
 console.log('Runner up: ...When you play the game of thrones, you win or you die.');
