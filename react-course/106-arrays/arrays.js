6614ecc78015e18a134376013f773eaa0b5b7759
Steph Warsh <swarsh@u.rochester.edu>
2019-11-05 09:51:02 -0500

    Cleaned up the code based on the instructor's solution

--- react-course/106-arrays/arrays.js
+++ react-course/106-arrays/arrays.js
@@ -1,31 +1,33 @@
 // convert the code below to use the array methods instead of for loops
 // you'll need to use the filter, reduce, map, and forEach methods
 
-const princesses = [{name: 'Rapunzel', age: 18}, {name: 'Mulan', age: 16},
-  {name: 'Anna', age: 18}, {name: 'Moana', age: 16}];
+const princesses = [
+  {name: 'Rapunzel', age: 18},
+  {name: 'Mulan', age: 16},
+  {name: 'Anna', age: 18},
+  {name: 'Moana', age: 16}
+];
 
 // log the name of each princess, follow by a colon, followed by their age
 princesses.forEach(princess => console.log(`${princess.name}: ${princess.age}`));
-
 
 // create an array of princess names from the existing array
 const names = princesses.map(princess => princess.name);
 console.log("names: ", names);
 
-
 // using the `names` array, get only those names that start with an 'M'
 const mNames = names.filter(name => name.startsWith('M'));
 console.log("m-names: ", mNames);
 
-
 // get a single value from the data: the average age of all of the princesses
-const sum = princesses.reduce((accumulator, currentValue) => accumulator + currentValue.age, 0);
+const sum = princesses.reduce((sum, princess) => sum + princess.age, 0);
 const average = sum / princesses.length;
 console.log("average age: ", average);
 
 
 // BONUS: get the average age of all princesses whose name includes an 'l'
 const lNames = princesses.filter(princess => princess.name.includes('l'));
-const lAverage = lNames.reduce((accumulator, currentValue) => accumulator + currentValue.age, 0) / lNames.length;
+const lSum = lNames.reduce((accumulator, currentValue) => accumulator + currentValue.age, 0);
+const lAverage = lSum / lNames.length;
 
 console.log("l-names average age: ", lAverage);
