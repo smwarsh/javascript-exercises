887379b1d6b4bcd4dea84401fbc563c02651a722
Steph Warsh <swarsh@u.rochester.edu>
2019-11-05 09:40:53 -0500

    Changed the for loops to forEach, map, filter, and reduce

--- react-course/106-arrays/arrays.js
+++ react-course/106-arrays/arrays.js
@@ -5,38 +5,21 @@ const princesses = [{name: 'Rapunzel', age: 18}, {name: 'Mulan', age: 16},
   {name: 'Anna', age: 18}, {name: 'Moana', age: 16}];
 
 // log the name of each princess, follow by a colon, followed by their age
-// can be replaced with forEach
-for (let i = 0; i < princesses.length; i++) {
-  console.log(`${princess[i].name}: ${princess[i].age}`);
-}
+princesses.forEach(princess => console.log(`${princess.name}: ${princess.age}`));
 
 
 // create an array of princess names from the existing array
-// can be replaced with map
-let names = [];
-for (let i = 0; i < princesses.length; i++) {
-  names.push(princesses[i].name);
-}
+const names = princesses.map(princess => princess.name);
 console.log("names: ", names);
 
 
 // using the `names` array, get only those names that start with an 'M'
-// can be replaced with filter
-let mNames = [];
-for (let i = 0; i < names.length; i++) {
-  if (names[i].startsWith('M')) {
-    mNames.push(names[i]);
-  }
-}
+const mNames = names.filter(name => name.startsWith('M'));
 console.log("m-names: ", mNames);
 
 
 // get a single value from the data: the average age of all of the princesses
-// Can be replaced with reduce
-let sum = 0;
-for (let i = 0; i < princesses.length; i++) {
-  sum += princesses[i].age;
-}
+let sum = princesses.reduce((accumulator, currentValue) => accumulator + currentValue.age, 0);
 const average = sum / princesses.length;
 console.log("average age: ", average);
 
